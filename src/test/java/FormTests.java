71a7fd486f06d7b27958d93a9fb0486aecef0db8
Dmitrii Vartik <dekesite@mail.ru>
2022-11-29 23:20:33 +0300

    added picture uploading and date picking

--- src/test/java/FormTests.java
+++ src/test/java/FormTests.java
@@ -3,6 +3,8 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
+import java.io.File;
+
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -27,12 +29,29 @@ public class FormTests {
 //        $(By.name("gender")).selectRadio("male"); // doesn't work
         $("[name=gender][value=Male]").parent().click();
         $("#userNumber").setValue("9876543210");
-        $("#currentAddress").setValue("Test address 123 building 0");
+
+
+        // select date of birth
         $("#dateOfBirthInput").click();
         $(".react-datepicker__year-select").selectOptionByValue("1987");
         $(".react-datepicker__month-select").selectOptionContainingText("June");
         $("[aria-label='Choose Sunday, June 21st, 1987']").click();
 
+        // select subjects
+        // ...
+
+        // select hobbies
+        $("[for=hobbies-checkbox-2]").click();
+        $("[for=hobbies-checkbox-3]").click();
+
+        // upload picture
+        $("#uploadPicture").uploadFile(new File("src/test/resources/me.jpg"));
+//        $("#uploadPicture").sen;
+
+        $("#currentAddress").setValue("Test address 123 building 0");
+
+        // select state and city
+        // ...
 
 //        $("[id=search]").shouldHave(text("https://selenide.org"));
     }
