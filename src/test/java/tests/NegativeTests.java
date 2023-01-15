4d72ca90762a6a616e6a2089f145b8c8137fb9f8
Dmitrii Vartik <dekesite@mail.ru>
2023-01-15 14:01:07 +0300

    added NegativeTests

---
+++ src/test/java/tests/NegativeTests.java
@@ -0,0 +1,12 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class NegativeTests {
+
+    @Test
+    void negativeAssertTest() {
+            assertTrue(false);
+    }
+}
